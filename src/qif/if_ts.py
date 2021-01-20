4098adf691ffb82fc806b8352524223007a074b4
NickQian <729480514@qq.com>
2021-01-20 18:06:57 +0800

    if_ts works

--- src/qif/if_ts.py
+++ src/qif/if_ts.py
@@ -12,14 +12,18 @@ import datetime
 import tushare as ts
 
 
-ts.set_token('your token here')
+ts.set_token('7aaef57854cac90bf596075e9cd60d0c256b62477e312aa509069fcd')
+pro = ts.pro_api()
+print("tushare pro version:", ts.__version__)
+
+# daily, eg: '000001.SH', '20181010'
+def GetK(index, startDay, endDay):
+	df = pro.index_daily(ts_code=index, start_date= startDay, end_date=endDay)
+	#print ("@:python info, df:", df)
+	kclose = df['close'].tolist()
+	return kclose
 
 
-# daily
-def GetK
 
-df = pro.index_daily(ts_code='399300.SZ')
-
-#或者按日期取
-
-df = pro.index_daily(ts_code='399300.SZ', start_date='20180101', end_date='20181010')
+if __name__ ==  "__main__":
+	print ("Try to get market data:", GetK("000001.SH","20190911", "20190916") )
