7f7893ceaf582dc45fa0b8516fc4d86a39b15a1a
NickQian <729480514@qq.com>
2021-01-25 20:25:14 +0800

    the k-flt works

--- src/pb/kflt.go
+++ src/pb/kflt.go
@@ -13,14 +13,25 @@ import (
 	"github.com/mjibson/go-dsp/window"
 	_ "github.com/mjibson/go-dsp/dsputils"
 	. "define"
-	_ "cmn"
+	"cmn"
+	"fmt"
+	"qif"
 	)
+
+
+func KlinePlot(indexCode, startDay, endDay string)bool{
+        kline := qif.GetKline( indexCode, startDay, endDay )
+        suc := PlotKl(kline, "Kline_org.png")
+        kl = KlinePreprc(kline)
+        suc = PlotKl(kl, "Kline_DcRmved.png")
+        if !suc{fmt.Printf("<KlinePlot>: %v \n", cmn.ErrPlotFail)   }
+	return suc
+}
 
 // Frequency domain Analyze
 func KlineFa(kl []float64)(Y_w []complex128){
 	fftRes := fft.FFTReal(kl)
-	//PlotFa(fftRes, "FreqAmp")
-
+	//PlotFa(fftRes, "FreqAmp"
 	return fftRes
 }
 
@@ -29,15 +40,19 @@ func KlineFa(kl []float64)(Y_w []complex128){
 func FltK(kl []float64)( kf []float64){
 	// 1) (y_t)- FFT -> Y_w
 	Y_w := KlineFa(kl)
+	PlotFa(Y_w[:30], "testFltK_Fa.png")
 
 	// 2) Gen win -> W_w
 	win := GenWin(len(kl), FFT_INTEREST_POINTS, window.Rectangular)  // Hamming/Hann/Bartlett/Rectangular/FlatTop/Blackman
+	//fmt.Printf("### 1: win###: %v  \n", win)
 
 	// 3) Multi -> YL_w
 	YL_w := MultYW(Y_w, win)
+	fmt.Printf("### 2 YL_w###:  %v   \n", YL_w)
 
-	// 4) iFFT -> yL_t
+	// 4) iFFT -> yL_t: complex out
 	yL_t := fft.IFFT(YL_w)          // IFFT(x []complex128)[]complex128
+	//fmt.Printf("### 3: yL_t###: v%   \n", yL_t)
 
 	// 5) last process
 	kf = GetCmplxAmp(yL_t)
--- src/pb/kflt_test.go
+++ src/pb/kflt_test.go
@@ -10,7 +10,7 @@ package pb
 import (
 	"testing"
 	"qif"
-	"fmt"
+	_ "fmt"
 	"cmn"
 	)
 
@@ -20,14 +20,15 @@ var kl []float64
 
 
 func init(){
-	kline := qif.GetKline("000001.SH", "20210101", "20210112")
+	//kline := qif.GetKline("000001.SH", "20190101", "20210112")
+	kline := qif.GetKline("000001.SH", "20000101", "20210120")
 	kl = KlinePreprc(kline)
-	fmt.Printf("<init>:kl is: %v \n", kl)
+	//fmt.Printf("<init>:kl is: %v \n", kl)
 }
 
 
 func TestKlinePlot(t *testing.T){
-	suc := PlotKl(kl, "test_Kline_org.png")
+	suc := KlinePlot("000001.SH", "20140601", "20210116")
 	if !suc{ t.Logf("<TestKlinePlot>: %v \n", cmn.ErrPlotFail)   }
 }
 
@@ -43,8 +44,10 @@ func TestKlineFa(t *testing.T){
 //
 func TestFltK(t *testing.T){
 	kfilted := FltK(kl)         // k line filtered, complex number
-	suc := PlotSlicef(kfilted, "time","Amp","testFltK_filtedKline.png")
-	if !suc {
-                t.Logf("Error: Someting error during <TestFltK>. suc is: %v", suc)
+	suc1 := PlotSlicef(kfilted, "time","Amp","testFltK_filtedKline.png")
+	suc2 := PlotKl(kl, "testFltK_orgKline.png")
+
+	if !(suc1 && suc2 ) {
+                t.Logf("Error: Someting error during <TestFltK>. suc1&suc2 are: %v, %v ", suc1, suc2)
         }
 }
